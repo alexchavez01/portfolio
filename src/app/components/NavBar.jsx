eecaa163672ac8fdd054c9b293a1c9a2f92ca349
Alex Chavez <achavez25@wooster.edu>
2024-04-18 20:17:31 -0400

    quick fixes

--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -51,8 +51,8 @@ const NavBar = () => {
             {!isFloating ? ( // Render this nav if not in floating mode.
                 <nav className="absolute top-0 left-0 right-0 z-10 px-10">
                     <div className="flex flex-wrap justify-between items-center mx-auto px-6 py-3 bg-[#12121212] bg-opacity-100">
-                        <Link href={'/'} className="text-2xl md:text-5xl text-white font-semibold">
-                            Logo 
+                        <Link href={'/'} className="text-xl md:text-2xl text-white font-semibold">
+                            alex
                         </Link>
                         <div className="menu hidden md:block md:w-auto" id="navbar">
                             <ul className="flex p-4 md:p-0 md:flex-row md:space-x-6 mt-0">
